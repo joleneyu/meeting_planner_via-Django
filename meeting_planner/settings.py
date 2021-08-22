07e91b43dba55fae235d7583f21a7cacbc82ba05
JoxFox <jingran.yu1988@gmail.com>
2021-08-22 22:43:10 +1000

    containerized the app and update mysql as app's database Signed-off-by: JoxFox <jingran.yu1988@gmail.com>

--- meeting_planner/settings.py
+++ meeting_planner/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'meeting_planner.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'meeting_planner',
+        'USER': 'root',
+        'PASSWORD': 'secret',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
 
